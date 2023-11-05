c5dc2ab1ad3c9750816a528209850de3160ecee8
SangamSid <sangam142@gmail.com>
2023-11-05 12:41:07 +0530

    final changes in env file

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,14 @@ const app=express();
 const mongoose=require("mongoose")
 const router=require("./router")
 const cors = require('cors')
+require("dotenv").config()
 app.use(cors())
 app.use(express.json());
 app.use("/api",router)
 
 
-mongoose.connect("mongodb://127.0.0.1:27017/bookmyshow",{useNewUrlParser:true,useUnifiedTopology:true}).
+
+mongoose.connect(process.env.MONGODB_URL,{useNewUrlParser:true,useUnifiedTopology:true}).
 then(()=>{
     console.log("connection to db success")
 }).catch((err)=>{
